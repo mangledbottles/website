1dee9eb40dec981c4a063946cbe44516ce9f0997
Dermot <contact@dermotobrien.me>
2021-09-04 22:13:15 +0100

    Add Stars to 3D background

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,20 @@ export default function Home() {
 
     scene.add(pointLight, ambientLight);
 
+    function addStar() {
+      const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+      const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      const star = new THREE.Mesh(geometry, material);
+
+      const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+      star.position.set(x, y, z);
+      scene.add(star);
+    }
+
+    Array(200).fill().forEach(addStar);
   })
   return (
 
