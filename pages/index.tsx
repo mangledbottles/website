87e281f92c867edeeb2bc0dd0c7c582539d98b1f
Dermot <contact@dermotobrien.me>
2021-09-04 22:12:04 +0100

    Setup Renderer and Camera

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,13 @@ export default function Home() {
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#bg'),
     });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(30);
+    camera.position.setX(-3);
+
+    renderer.render(scene, camera);
   })
   return (
 
