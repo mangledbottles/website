75b7f37646649242b9203e3ffa27ee63fc993b62
Dermot <contact@dermotobrien.me>
2021-10-13 14:16:24 +0100

    Remove icons for the moment

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,10 +20,10 @@ export default function Projects() {
                                 <h3>TikTok API - Reverse Engineering Private API</h3>
 
                                 <div className="icon-text">
-                                    <span className="icon has-text-info">
+                                    {/* <span className="icon has-text-info"> */}
                                         {/* <i className="fas fa-info-circle"></i> */}
-                                        <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="external-link-alt" className="svg-inline--fa fa-external-link-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g className="fa-group"><path className="fa-secondary" fill="currentColor" d="M400 320h32a16 16 0 0 1 16 16v128a48 48 0 0 1-48 48H48a48 48 0 0 1-48-48V112a48 48 0 0 1 48-48h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H64v320h320V336a16 16 0 0 1 16-16z" opacity="0.4"></path><path className="fa-primary" fill="currentColor" d="M512 24v128c0 21.47-26 32-41 17l-35.71-35.71L191.8 376.77a24 24 0 0 1-33.94 0l-22.63-22.63a24 24 0 0 1 0-33.94L378.76 76.68 343.05 41C328 25.9 338.66 0 360 0h128a24 24 0 0 1 24 24z"></path></g></svg>
-                                    </span>
+                                        {/* <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="external-link-alt" className="svg-inline--fa fa-external-link-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g className="fa-group"><path className="fa-secondary" fill="currentColor" d="M400 320h32a16 16 0 0 1 16 16v128a48 48 0 0 1-48 48H48a48 48 0 0 1-48-48V112a48 48 0 0 1 48-48h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H64v320h320V336a16 16 0 0 1 16-16z" opacity="0.4"></path><path className="fa-primary" fill="currentColor" d="M512 24v128c0 21.47-26 32-41 17l-35.71-35.71L191.8 376.77a24 24 0 0 1-33.94 0l-22.63-22.63a24 24 0 0 1 0-33.94L378.76 76.68 343.05 41C328 25.9 338.66 0 360 0h128a24 24 0 0 1 24 24z"></path></g></svg> */}
+                                    {/* </span> */}
                                     <a href="https://github.com/mangledbottles/Musically-API" target="_blank"><span>https://github.com/mangledbottles/Musically-API</span></a>
                                 </div>
 
@@ -37,10 +37,10 @@ export default function Projects() {
                                 <h3>End-to-End Encryption Screen Capture</h3>
 
                                 <div className="icon-text">
-                                    <span className="icon has-text-info">
+                                    {/* <span className="icon has-text-info"> */}
                                         {/* <i className="fas fa-info-circle"></i> */}
-                                        <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="external-link-alt" className="svg-inline--fa fa-external-link-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g className="fa-group"><path className="fa-secondary" fill="currentColor" d="M400 320h32a16 16 0 0 1 16 16v128a48 48 0 0 1-48 48H48a48 48 0 0 1-48-48V112a48 48 0 0 1 48-48h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H64v320h320V336a16 16 0 0 1 16-16z" opacity="0.4"></path><path className="fa-primary" fill="currentColor" d="M512 24v128c0 21.47-26 32-41 17l-35.71-35.71L191.8 376.77a24 24 0 0 1-33.94 0l-22.63-22.63a24 24 0 0 1 0-33.94L378.76 76.68 343.05 41C328 25.9 338.66 0 360 0h128a24 24 0 0 1 24 24z"></path></g></svg>
-                                    </span>
+                                        {/* <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="external-link-alt" className="svg-inline--fa fa-external-link-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g className="fa-group"><path className="fa-secondary" fill="currentColor" d="M400 320h32a16 16 0 0 1 16 16v128a48 48 0 0 1-48 48H48a48 48 0 0 1-48-48V112a48 48 0 0 1 48-48h160a16 16 0 0 1 16 16v32a16 16 0 0 1-16 16H64v320h320V336a16 16 0 0 1 16-16z" opacity="0.4"></path><path className="fa-primary" fill="currentColor" d="M512 24v128c0 21.47-26 32-41 17l-35.71-35.71L191.8 376.77a24 24 0 0 1-33.94 0l-22.63-22.63a24 24 0 0 1 0-33.94L378.76 76.68 343.05 41C328 25.9 338.66 0 360 0h128a24 24 0 0 1 24 24z"></path></g></svg> */}
+                                    {/* </span> */}
                                     <a href="https://github.com/mangledbottles/Screencapture-Encrypted" target="_blank"><p>https://github.com/mangledbottles/Screencapture-Encrypted</p></a>
                                 </div>
 
