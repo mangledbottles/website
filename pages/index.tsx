4c57a6a96515c826d5cdc19814ba00e899804760
Dermot <contact@dermotobrien.me>
2021-10-13 19:40:18 +0100

    Remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Loader from '../components/Loader';
 import Resume from '../components/Resume';
 import Contact from '../components/Contact';
 import 'bulma/css/bulma.min.css';
